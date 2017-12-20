55583e7d4cb844a7d7244224a5e43ec96db3e85b
cao.fu@monstar-lab-cn.com <cao.fu@monstar-lab-cn.com>
2017-12-20 14:18:41 +0800

    缓存测试
    Signed-off-by: cao.fu@monstar-lab-cn.com <cao.fu@monstar-lab-cn.com>

--- src/test/java/cn/wscq/spring/domain/service/cache/CacheServiceTest.java
+++ src/test/java/cn/wscq/spring/domain/service/cache/CacheServiceTest.java
@@ -1,11 +1,27 @@
 package cn.wscq.spring.domain.service.cache;
 
 import cn.wscq.spring.MyTestsConfiguration;
+import cn.wscq.spring.domain.model.form.DemoForm;
+import cn.wscq.spring.domain.model.mybatis.TDemo;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.sun.org.apache.xpath.internal.SourceTree;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.TimeoutUtils;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Collection;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.*;
 
@@ -20,31 +36,83 @@ import static org.junit.Assert.*;
 @Import(MyTestsConfiguration.class)
 public class CacheServiceTest {
 
+    @Autowired
+    private CacheService cacheService;
+
     @Test
     public void saveString() {
+        //存贮一般的字符串
+        cacheService.saveString("stringone", "zhansan");
+        Assert.assertEquals("zhansan",cacheService.getString("stringone"));
+
     }
 
     @Test
     public void saveString1() {
+        try {
+            //存贮字符串并且设置超时
+            cacheService.saveString("stringtwo","lisi",10);
+            Thread.sleep(11000);
+            Assert.assertEquals(null,cacheService.getString("stringtwo"));
+        }
+        catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
     }
 
     @Test
     public void saveObject() {
+
+        TDemo demo = new TDemo();
+        demo.setTestId(2L);
+        demo.setTestName("hahah");
+        cacheService.saveObject("objectone",demo);
+        //获取到demo对象
+        TDemo demo2 = cacheService.getObject("objectone",TDemo.class);
+        Assert.assertEquals(demo,demo2);
+
     }
 
     @Test
     public void saveObject1() {
+        TDemo demo = new TDemo();
+        demo.setTestId(1L);
+        demo.setTestName("haha");
+        try {
+            //存贮对象并且设置超时
+            cacheService.saveObject("objecttwo",demo,10);
+            Thread.sleep(11000);
+            Assert.assertEquals(null,cacheService.getObject("objecttwo",TDemo.class));
+        }
+        catch (InterruptedException e){
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void getString() {
+        String value = cacheService.getString("stringone");
+        Assert.assertEquals("zhansan",value);
+
     }
 
     @Test
     public void getObject() {
+        TDemo demo2 = new TDemo();
+        demo2.setTestId(2L);
+        demo2.setTestName("hahah");
+        //获取到demo对象
+        TDemo demo3 = cacheService.getObject("objectone",TDemo.class);
+        Assert.assertEquals(demo2,demo3);
+
     }
 
     @Test
     public void delete() {
+        cacheService.delete("stringone");
+        Assert.assertEquals(null,cacheService.getString("stringone"));
     }
+
+
 }
