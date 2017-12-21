46945da39669bc32ae841702b85aef8fe75dad9f
cao.fu@monstar-lab-cn.com <cao.fu@monstar-lab-cn.com>
2017-12-21 15:40:41 +0800

    redistest测试修改

--- src/test/java/cn/wscq/spring/domain/service/cache/CacheServiceTest.java
+++ src/test/java/cn/wscq/spring/domain/service/cache/CacheServiceTest.java
@@ -1,29 +1,15 @@
 package cn.wscq.spring.domain.service.cache;
 
 import cn.wscq.spring.MyTestsConfiguration;
-import cn.wscq.spring.domain.model.form.DemoForm;
 import cn.wscq.spring.domain.model.mybatis.TDemo;
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.TypeReference;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.sun.org.apache.xpath.internal.SourceTree;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.TimeoutUtils;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.Collection;
-import java.util.concurrent.TimeUnit;
-
-import static org.junit.Assert.*;
 
 /**
  * @author shuai
@@ -39,11 +25,25 @@ public class CacheServiceTest {
     @Autowired
     private CacheService cacheService;
 
+
+    @Before
+    public void beforeTest() {
+
+        cacheService.saveString("stringone", "zhansan");
+    }
+
     @Test
     public void saveString() {
         //存贮一般的字符串
         cacheService.saveString("stringone", "zhansan");
         Assert.assertEquals("zhansan",cacheService.getString("stringone"));
+
+    }
+
+    @Test
+    public void getString() {
+        String value = cacheService.getString("stringone");
+        Assert.assertEquals("zhansan",value);
 
     }
 
@@ -67,10 +67,14 @@ public class CacheServiceTest {
         TDemo demo = new TDemo();
         demo.setTestId(2L);
         demo.setTestName("hahah");
-        cacheService.saveObject("objectone",demo);
+        cacheService.saveObject("objectone", demo);
         //获取到demo对象
-        TDemo demo2 = cacheService.getObject("objectone",TDemo.class);
-        Assert.assertEquals(demo,demo2);
+        TDemo demo2 = cacheService.getObject("objectone", TDemo.class);
+        //分别取出对象里面的字段进行Assert
+        Assert.assertEquals(2L, Integer.parseInt(demo2.getTestId().toString()));
+        Assert.assertEquals("hahah", demo2.getTestName().toString());
+
+        //Assert.assertEquals(demo,demo2);
 
     }
 
@@ -90,21 +94,20 @@ public class CacheServiceTest {
         }
     }
 
-    @Test
-    public void getString() {
-        String value = cacheService.getString("stringone");
-        Assert.assertEquals("zhansan",value);
 
-    }
 
     @Test
     public void getObject() {
-        TDemo demo2 = new TDemo();
+       /* TDemo demo2 = new TDemo();
         demo2.setTestId(2L);
-        demo2.setTestName("hahah");
+        demo2.setTestName("hahah");*/
         //获取到demo对象
         TDemo demo3 = cacheService.getObject("objectone",TDemo.class);
-        Assert.assertEquals(demo2,demo3);
+        //分别取出对象里面的字段进行Assert
+        Assert.assertEquals(2L, Integer.parseInt(demo3.getTestId().toString()));
+        Assert.assertEquals("hahah", demo3.getTestName().toString());
+
+        //Assert.assertEquals(,demo3);
 
     }
 
